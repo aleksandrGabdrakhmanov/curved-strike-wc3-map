245047cf6cca56b07f5bc80842859f8bd6af0fe3
Aleksandr.Gabdrakhmanov <Aleksandr.Gabdrakhmanov@leroymerlin.ru>
2023-12-06 16:47:14 +0300

    delete synced-table

--- src/game/init/global-variables-init.lua
+++ src/game/init/global-variables-init.lua
@@ -13,9 +13,9 @@ function initAllTeamsAndPlayers()
         },
         spawnInterval = 30
     }
-    all_teams = SyncedTable {
+    all_teams = {
         {
-            players = SyncedTable {
+            players = {
                 {
                     id = Player(4),
                     income = game_config.economy.startIncomePerSec,
@@ -52,11 +52,11 @@ function initAllTeamsAndPlayers()
                     attackPointRect = { gg_rct_spawn_left_down, gg_rct_attack_region_p10, gg_rct_attack_region_center_right, gg_rct_attack_region_p2 }
                 }
             },
-            spawnPlayers = SyncedTable { Player(15), Player(16), Player(17), Player(18), Player(19), Player(21), Player(23) },
+            spawnPlayers = { Player(15), Player(16), Player(17), Player(18), Player(19), Player(21), Player(23) },
             base = { player = Player(16), unitId = "ofrt", winTeam = 2 }
         },
         {
-            players = SyncedTable {
+            players = {
                 {
                     id = Player(8),
                     income = game_config.economy.startIncomePerSec,
@@ -93,7 +93,7 @@ function initAllTeamsAndPlayers()
                     attackPointRect = { gg_rct_spawn_right_down, gg_rct_attack_region_p6, gg_rct_attack_region_center_left, gg_rct_attack_region_p1 }
                 }
             },
-            spawnPlayers = SyncedTable { Player(10), Player(11), Player(12), Player(13), Player(14), Player(20), Player(22) },
+            spawnPlayers = { Player(10), Player(11), Player(12), Player(13), Player(14), Player(20), Player(22) },
             base = { player = Player(12), unitId = "ofrt", winTeam = 1 }
         }
     }
