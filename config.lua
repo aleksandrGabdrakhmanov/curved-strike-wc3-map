746bfe144eebfa19126968db8d271787e5d9c1ca
Aleksandr.Gabdrakhmanov <Aleksandr.Gabdrakhmanov@leroymerlin.ru>
2024-01-23 09:53:31 +0300

    releative path to build

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 require 'build' {
     game = nil,
-    project = '/home/gabik/projects/curved-strike-wc3-map',
+    project = '../curved-strike-wc3-map',
     map = 'map.w3m',
     src = 'src',
     run = 'launch_nothing',
